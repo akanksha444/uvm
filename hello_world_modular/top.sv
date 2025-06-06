4ad765894555aabb38b86d6c89a4967561cb6555
akanksha444 <156953063+akanksha444@users.noreply.github.com>
2025-06-05 19:42:04 -0700

    Create top.sv

---
+++ hello_world_modular/top.sv
@@ -0,0 +1,14 @@
+//top is the entry point 
+//we must call the run_test function here
+
+//run test a uvm method, it needs to have access to the uvm library
+//hence we must include the packages and macro files
+
+`include "test.sv"
+`include "uvm_macros.svh"
+
+module top;
+  initial begin
+    run_test();
+  end
+endmodule : top
